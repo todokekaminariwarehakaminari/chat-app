e77528e4b9fd4028ba74c208aa87bb2ba60c50aa
todokekaminariwarehakaminari <huangmuyuer@gmail.com>
2023-12-13 17:53:22 +0900

    messagesテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_11_183218) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_12_185339) do
+  create_table "messages", charset: "utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "room_users", charset: "utf8", force: :cascade do |t|
     t.bigint "room_id", null: false
     t.bigint "user_id", null: false
